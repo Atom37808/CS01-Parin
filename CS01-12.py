dd2cdc08150730fe19fb9354628d34601fefae64
Atom37808 <37808@virtual.prc.ac.th>
2021-08-11 11:37:10 +0700

    CS01-12.py

---
+++ CS01-12.py
@@ -0,0 +1,15 @@
+name = input("What is your name?")
+familyName = input('What is your family name?')
+studentNumber = input('What is your student number?')
+
+info = [name,familyName,studentNumber]
+
+i = info[0]
+i1= info[1]
+i2= info[2]
+
+print(i)
+print(i1)
+print(i2)
+
+# *sad face*
